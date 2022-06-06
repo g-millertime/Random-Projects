638e9440dd0965a6079c6966a2925541ef9d4d07
Garrett Miller <garrett.miller23@yahoo.com>
2022-06-06 15:58:46 -0500

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -9,7 +9,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-total_characters = nr_letters + nr_symbols + nr_numbers
 password = []   # set password as list object for random.shuffle method
 
 # Generate amount of characters of type
